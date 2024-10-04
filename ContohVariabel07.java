ee4eb0ca0375904d37b75769bf75739d74841495
esasyanii <syaniday@gmail.com>
2024-10-04 10:21:04 +0700

    Add files via upload

---
+++ ContohVariabel07.java
@@ -0,0 +1,15 @@
+public class ContohVariabel07 {
+    public static void main (String[] args) {
+        String SalahSatuHobySayaAdalah = "Membaca";
+        boolean isPandai = true;
+        char jenisKelamin = 'P';
+        byte _umurSayaSekarang = 18;
+        double $ipk = 3.91, tinggi = 1.58;
+        System.out.println(SalahSatuHobySayaAdalah);
+        System.out.println("Apakah pandai?" + isPandai);
+        System.out.println("Jenis kelamin: " + jenisKelamin);
+        System.out.println("Umurku saat ini: " + _umurSayaSekarang);
+        System.out.println(String.format("Saya beripk %s, dengan tinggi badan %s", $ipk, tinggi));
+
+    }
+}
